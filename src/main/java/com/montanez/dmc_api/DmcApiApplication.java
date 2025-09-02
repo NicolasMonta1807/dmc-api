6817025b390381d3472cd5bb100d3054bc4c67ad
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-09-02 18:02:23 -0500

    initial commit

---
+++ src/main/java/com/montanez/dmc_api/DmcApiApplication.java
@@ -0,0 +1,13 @@
+package com.montanez.dmc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcApiApplication.class, args);
+	}
+
+}
